1b75a4198cf445c82e7500cfd39e77dd00d58e3b
Gonzalo Montes <gonzamontes14a@gmail.com>
2023-02-02 14:26:00 -0300

    Finish

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'usuario/bienesraices',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9d99214b895ba48afcc9fdd54e2e3763eae2281a',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'usuario/bienesraices' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9d99214b895ba48afcc9fdd54e2e3763eae2281a',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
